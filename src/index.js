d92c755b810506c2e799ccec23e17f37dc37f5f4
FabianoLuizonCampos <fabianoluizoncampos@gmail.com>
2021-03-23 15:35:15 -0300

    chore: create app.post('/users', ...),  app.get('/todos', ...),  app.post('/todos', ...) and function checksExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -1,29 +1,80 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( user => user.username === username );
+
+  if(!user) {
+    return response.status(404).json({
+      error: "User not exists!!"
+    })
+  }
+
+  request.user = user;
+  //request.username = username;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username } = request.body;
+
+  const usernameAlreadyExists = users.some( user => user.username === username );
+
+  if(usernameAlreadyExists) {
+      return response.status(400).json( {
+          error: "Username already exists!!"
+      });
+  } 
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,   
+    todos: []
+  });
+
+  return response.status(201).json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const todosInsertion = {
+    id: uuidv4(),
+    title,
+    done: false,  
+    deadline: new Date(deadline),
+    created_at: new Date()    
+  };
+
+  user.todos.push(todosInsertion);
+
+  return response.status(201).json(user.todos)
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
